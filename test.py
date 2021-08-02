e84de52f5217f281a9ca5f6f1652a5201aa38aa7
ansuho <86389163+ansuho@users.noreply.github.com>
2021-08-02 21:18:11 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print('Hello world')
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://ridibooks.com/category/new-releases/2200?order=selling'
+response = requests.get(url)
+response.encoding = 'utf-8'
+html = response.text
+
+soup = BeautifulSoup(html, 'html.parser')
+
+bookservice = soup.select('.title_text')
+for no, book in enumerate(bookservice, 1):
+  print(no, book.text.strip())
